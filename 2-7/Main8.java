b36d1976728093bf41c0d7fcd96fedba8754e423
michiru-ida <idam66m@gmail.com>
2026-05-29 08:34:40 +0900

    Add files via upload

--- 2-7/Main8.java
+++ 2-7/Main8.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Main8 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
+
         System.out.println("攻撃力を入力してください:");
-        int atk=scanner.nextInt(50);
+        int atk=scanner.nextInt();
 
         System.out.println("防御力を入力してください:");
-        int def=scanner.nextInt(20);
-        double damage=atk-def;
+        int def=scanner.nextInt();
+        int damage=atk-def;
         int criticaldamage=(int)(damage*1.5);
         System.out.println("通常ダメージ:"+damage);
         System.out.println("クリティカルダメージ:"+criticaldamage);
