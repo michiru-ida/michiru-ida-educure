4ff0b99251f3684aa94b3d6e9bb08f25b7b9e75d
michiru-ida <idam66m@gmail.com>
2026-05-30 22:45:46 +0900

    Add files via upload

--- 2-7/Main6.java
+++ 2-7/Main6.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main6 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
+        System.out.print("現在の経験値を入力してください:");
         int exp=scanner.nextInt();
         int bonus=(int)(exp+exp*0.25);
-        System.out.println("現在の経験値を入力してください:"+exp);
         System.out.println("ボーナス後の経験値:"+bonus);
         scanner.close();
     }
