7cf17c6ade858a07c27e3238d24b95b2d201e356
michiru-ida <idam66m@gmail.com>
2026-06-02 09:11:00 +0900

    Add files via upload

--- 2-7/Main3.java
+++ 2-7/Main3.java
@@ -2,6 +2,7 @@ public class Main3 {
     public static void main(String[] args) {
         int power=15;
         int defence=5;
+        //小数点以下が切り捨てとなるようにしてください
         int damage=(power*2-defence)/3;
       System.out.println("与えるダメージ："+damage);  
     }
--- 2-7/Main6.java
+++ 2-7/Main6.java
@@ -4,7 +4,8 @@ public class Main6 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         System.out.print("現在の経験値を入力してください:");
-        int exp=scanner.nextInt();
+        String input=scanner.nextLine();
+        int exp=Integer.parseInt(input);
         int bonus=(int)(exp+exp*0.25);
         System.out.println("ボーナス後の経験値:"+bonus);
         scanner.close();
