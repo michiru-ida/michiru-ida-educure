54765bc80b966cfa0f337b8707d155f3af7a4057
michiru-ida <idam66m@gmail.com>
2026-07-07 10:56:33 +0900

    Add files via upload

--- 2-12/Main3.java
+++ 2-12/Main3.java
@@ -1,9 +1,16 @@
 public class Main3 {
     public static void main(String[] args) {
         for(int i=1;i<=5;i++){
-            for(int height=1;height<=5;height++){
-            System.out.println("*"+(i*height));
+            int height=5;
+            for(int space=1;space<=height-i;space++){
+                System.out.print(" ");
+            }
+            for(int star=1;star<=2*i-1;star++)
+            System.out.print("*");
+        System.out.println();
         }
-    }
-    }
+     }
 }
+            
+            
+
